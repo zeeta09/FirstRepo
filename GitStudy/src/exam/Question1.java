4992d3313bed4f662883ff01b48108b6faef7adf
zeeta <zeeta@LAPTOP-3OHSUGVC>
2021-04-19 11:34:58 +0900

    문제1 풀고 저장

---
+++ GitStudy/src/exam/Question1.java
@@ -0,0 +1,11 @@
+package exam;
+
+public class Question1 {
+	public static void main(String[] args) {
+		System.out.println(" /)/)");
+		System.out.println("( ..)");
+		System.out.println("( >¢½");
+		
+	}
+
+}
