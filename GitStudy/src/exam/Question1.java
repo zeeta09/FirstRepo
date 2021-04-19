da3bb0464d6fc5fa582dca8cd415bc29f621c68c
zeeta <zeeta@LAPTOP-3OHSUGVC>
2021-04-19 11:36:09 +0900

    문제1 수정한 후 저장

--- GitStudy/src/exam/Question1.java
+++ GitStudy/src/exam/Question1.java
@@ -3,8 +3,8 @@ package exam;
 public class Question1 {
 	public static void main(String[] args) {
 		System.out.println(" /)/)");
-		System.out.println("( ..)");
-		System.out.println("( >¢½");
+		System.out.println("( xx)");
+		System.out.println("( >$");
 		
 	}
 
